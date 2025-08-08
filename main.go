c4c201a32777910242756e2fd82b391b04dff1ca
Rickey Ingrassia <splitnines@gmail.com>
2025-08-08 09:50:01 -0700

    refactor: keeping it old school with 80 character lines, max

--- main.go
+++ main.go
@@ -21,7 +21,8 @@ func HttpSrv() {
 
 	ip := getLocalIP()
 	port := ":12345"
-	fmt.Printf("[+] Running Extended HTTP Server on %s port %s\n", ip, port[1:])
+	fmt.Printf("[+] Running Extended HTTP Server on %s port %s\n",
+		ip, port[1:])
 	fmt.Printf("[+] Server URL: (http://%s%s/)\n", ip, port)
 	fmt.Println("[+] Press Ctrl-c to stop the server")
 	log.Fatal(http.ListenAndServe(port, nil))
@@ -34,7 +35,8 @@ func fileHandler(w http.ResponseWriter, r *http.Request) {
 	case http.MethodPut:
 		Put(w, r)
 	default:
-		http.Error(w, "Unsupported method", http.StatusMethodNotAllowed)
+		http.Error(w, "Unsupported method",
+			http.StatusMethodNotAllowed)
 	}
 }
 
@@ -47,24 +49,28 @@ func Put(w http.ResponseWriter, r *http.Request) {
 	path := filepath.Clean("." + r.URL.Path)
 
 	if strings.HasSuffix(r.URL.Path, "/") {
-		http.Error(w, "Don't even try to put that directory", http.StatusBadRequest)
+		http.Error(w, "Don't even try to put that directory",
+			http.StatusBadRequest)
 		return
 	}
 
 	if err := os.MkdirAll(filepath.Dir(path), 0755); err != nil {
-		http.Error(w, "Failed to create directory: "+err.Error(), http.StatusInternalServerError)
+		http.Error(w, "Failed to create directory: "+err.Error(),
+			http.StatusInternalServerError)
 		return
 	}
 
 	file, err := os.Create(path)
 	if err != nil {
-		http.Error(w, "Shit, failed to create the file: "+err.Error(), http.StatusInternalServerError)
+		http.Error(w, "Shit, failed to create the file: "+err.Error(),
+			http.StatusInternalServerError)
 		return
 	}
 	defer file.Close()
 
 	if _, err := io.Copy(file, r.Body); err != nil {
-		http.Error(w, "Shit, failed to write file: "+err.Error(), http.StatusInternalServerError)
+		http.Error(w, "Shit, failed to write file: "+err.Error(),
+			http.StatusInternalServerError)
 		return
 	}
 	w.WriteHeader(http.StatusCreated)
@@ -86,10 +92,13 @@ func logRequest(next http.HandlerFunc) http.HandlerFunc {
 		start := time.Now()
 		ip, _, _ := net.SplitHostPort(r.RemoteAddr)
 
-		lrw := &loggingResponseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+		lrw := &loggingResponseWriter{
+			ResponseWriter: w, statusCode: http.StatusOK,
+		}
 		next.ServeHTTP(lrw, r)
 
-		log.Printf("[%s] %s %s - %d (%s)", ip, r.Method, r.URL.Path, lrw.statusCode, time.Since(start))
+		log.Printf("[%s] %s %s - %d (%s)", ip, r.Method, r.URL.Path,
+			lrw.statusCode, time.Since(start))
 	}
 }
 
